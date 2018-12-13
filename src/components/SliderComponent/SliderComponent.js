a4ca7dafc89251a99e44e1751f0280703d42dceb
Дорофеев Александр Николаевич <a.dorofeev@informika.ru>
2018-12-13 08:52:04 +0300

    play with store

--- src/components/SliderComponent/SliderComponent.js
+++ src/components/SliderComponent/SliderComponent.js
@@ -3,20 +3,81 @@ import './SliderComponent.scss';
 
 import SliderCard from '../SliderCardComponent/SliderCardComponent';
 
-class Slider extends Component {
+// class Slider extends Component {
   
 
+//   render() {
+//     return (
+//       <div className="slider">
+//         <button className="Slider__btn">prev</button>
+//         <button className="Slider__btn">next</button>
+//         <div className="Slider__content">
+//           <SliderCard />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+const { Provider, Consumer } = React.createContext()
+
+const ActionTypes = {
+  TOGGLE: "TOGGLE"
+};
+
+const initialState = {
+  user: {
+    name: 'Alex'
+  },
+  isADuck: false
+};
+
+const withState = WrappedComponent => props => (
+  <Consumer>{state => <WrappedComponent {...state} {...props} />}</Consumer>
+);
+
+const reducer = (state, action) => {
+  if (action.type === ActionTypes.TOGGLE) {
+    console.log(state.isADuck)
+    return { ...state, isADuck: !state.isADuck };
+  }
+  console.log(state.isADuck)
+};
+
+
+class StateProvider extends Component {
+  state = {
+    ...this.props.initialState,
+    dispatch: action => {
+      this.setState(state => reducer(state, action));
+    },
+    // clickHandler: this.state.dispatch({ type: ActionTypes.TOGGLE })
+  }
+
   render() {
+    const { state, props: { children } } = this;
+
     return (
-      <div className="slider">
-        <button className="Slider__btn">prev</button>
-        <button className="Slider__btn">next</button>
-        <div className="Slider__content">
-          <SliderCard />
-        </div>
-      </div>
-    );
+      <Provider value={state}>{children}</Provider>
+    )
   }
 }
 
-export default Slider;
+const Welcome = withState(({ user, clickHandler, dispatch  }) => {
+  return (
+    <div>
+      <button onClick={e => dispatch({ type: ActionTypes.TOGGLE })}>Hello, {user.name}!</button>
+    </div>
+  )});
+
+class SliderProvider extends Component {
+  render() {
+    return (
+      <StateProvider initialState={initialState}>
+        <Welcome />
+        {/* {this.props.children} */}
+      </StateProvider>
+    )
+  }
+}
+
+export default SliderProvider;
--- src/contexts/sliderContext.js
+++ src/contexts/sliderContext.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react';
 import data from '../data/data';
 
-const SliderContext = React.createContext()
+const { Provider, Consumer } = React.createContext()
+
+const ActionTypes = {
+  TOGGLE: "TOGGLE"
+};
+
+const initialState = {
+  user: {
+    name: 'Alex'
+  },
+  isADuck: false
+};
+
+const withState = WrappedComponent => props => (
+  <Consumer>{state => <WrappedComponent {...state} {...props} />}</Consumer>
+);
+
+const reducer = (state, action) => {
+  if (action.type === ActionTypes.TOGGLE) {
+    return { ...state, isADuck: !state.isADuck };
+  }
+};
+
+
+class StateProvider extends Component {
+  state = {
+    ...this.props.initialState,
+    dispatch: action => {
+      this.setState(state => reducer(state, action));
+    },
+    clickHandler: this.state.dispatch({ type: ActionTypes.TOGGLE })
+  }
+  render() {
+    const { state, props: { children } } = this;
+
+    return (
+      <Provider value={state}>{children}</Provider>
+    )
+  }
+}
+
+const Welcome = withState(({ user }) => {
+  return (
+    <div>
+      <button onClick={}>Hello, {user.name}!</button>
+    </div>
+  )});
 
 class SliderProvider extends Component {
-  state = {
-    sliderData: data.properties,
-    activeSlide: data.properties[0]
-  }
-
   render() {
     return (
-      <SliderContext.Provider>
-        {this.props.children}
-      </SliderContext.Provider>
+      <StateProvider>
+        <Welcome />
+        {/* {this.props.children} */}
+      </StateProvider>
     )
   }
 }
